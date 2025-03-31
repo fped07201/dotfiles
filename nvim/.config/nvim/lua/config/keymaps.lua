c3994f7980cb347347ad19fb159e6361692356d9
Ferran Pedrera <ferran.pedrera@technica-engineering.de>
2025-03-31 10:12:48 +0200

    Update live grep with args

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -17,4 +17,6 @@ vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
 
 vim.keymap.set("n", "U", "<C-r>", { noremap = true, silent = true })
+vim.keymap.set("v", "<C-c>", '"+y', { noremap = true, silent = true })
+
 
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,14 +1,23 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope-live-grep-args.nvim'
+    },
     config = function()
+        local telescope = require('telescope')
+        telescope.setup {
+            extensions = {
+                live_grep_args = {}
+            }
+        }
+        telescope.load_extension('live_grep_args')
         -- Map Ctrl + P to Telescope's find_files function
         vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>", { noremap = true, silent = true })
-        -- Ctrl + F for live grep (search text in files)
-        vim.keymap.set("n", "<C-f>", ":Telescope live_grep<CR>", { noremap = true, silent = true })
+        -- Ctrl + F for live grep args (allows custom arguments)
+        vim.keymap.set("n", "<C-f>", ":Telescope live_grep_args<CR>", { noremap = true, silent = true })
         -- Ctrl + B for buffers
         vim.keymap.set("n", "<C-b>", ":Telescope buffers<CR>", { noremap = true, silent = true })
     end
 }
-
 
