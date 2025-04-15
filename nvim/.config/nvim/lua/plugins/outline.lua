a64caad3353b0a09ddb32895e77e1d4e30d1f623
Ferran Pedrera <ferran.pedrera@technica-engineering.de>
2025-04-15 13:26:21 +0200

    Add outline

---
+++ nvim/.config/nvim/lua/plugins/outline.lua
@@ -0,0 +1,9 @@
+return {
+  "simrat39/symbols-outline.nvim",
+  cmd = { "SymbolsOutline" },
+  keys = {
+    { "<leader>o", "<cmd>SymbolsOutline<CR>", desc = "Open Symbols Outline" },
+  },
+  config = true,
+}
+
