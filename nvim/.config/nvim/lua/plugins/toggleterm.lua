2fe43967cd70bdca9743bff00dd867e211837079
Ferran Pedrera <ferran.pedrera@technica-engineering.de>
2025-04-28 11:51:10 +0200

    Update

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -1,18 +1,19 @@
 return {
-    'akinsho/toggleterm.nvim', version = "*",
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    enabled = true,
     config = function()
-      require("toggleterm").setup{
-        size = 20,  -- Optional: Adjust terminal size
-        open_mapping = [[<C-t>]],  -- Set the keybinding to Ctrl + t
-        direction = "horizontal",  -- You can set it to "vertical", "float", or "horizontal"
-	shade_terminals = false,
-      }
+        require("toggleterm").setup {
+            size = 20,            -- Optional: Adjust terminal size
+            open_mapping = [[<C-t>]], -- Set the keybinding to Ctrl + t
+            direction = "horizontal", -- You can set it to "vertical", "float", or "horizontal"
+            shade_terminals = false,
+        }
 
-    -- Set terminal background color
-    vim.cmd([[ 
-      highlight TermBg guibg=#1e1e1e 
+        -- Set terminal background color
+        vim.cmd([[
+      highlight TermBg guibg=#1e1e1e
       highlight TermCursor guifg=#ffffff guibg=#ff0000
     ]])
     end
 }
-
