9b1b6fd854b17ff398e997182926e2c98b8bbc31
Ferran Pedrera <ferran.pedrera@technica-engineering.de>
2025-05-22 08:28:05 +0200

    Add function to comment C/C++ code with either // or /* */

--- nvim/.config/nvim/lua/plugins/vim-commentary.lua
+++ nvim/.config/nvim/lua/plugins/vim-commentary.lua
@@ -1,5 +1,18 @@
 return {
-    "tpope/vim-commentary",
-    lazy = false,
+  "tpope/vim-commentary",
+  lazy = false,
+
+  config = function()
+    -- Set `//` for C and C++
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "c", "cpp" },
+      callback = function()
+        vim.bo.commentstring = "// %s"
+      end,
+    })
+
+    -- If you want `/** %s */` instead, use:
+    -- vim.bo.commentstring = "/** %s */"
+  end,
 }
 
