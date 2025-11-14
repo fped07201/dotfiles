c86b37e4fa9214593eacda84cea82c006dd378e2
Ferran Pedrera <ferran.pedrera@technica-engineering.de>
2025-11-14 14:22:21 +0100

    Update Oil with keymap

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,11 +1,13 @@
 return {
     'stevearc/oil.nvim',
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {},
-    -- Optional dependencies
-    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-    lazy = false,
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    keys = {
+        { "-", function() require("oil").open() end, mode = { 'n' }, desc = "Open parent dir" },
+    },
+    opts = {
+        default_file_explorer = true,
+        view_options = {
+            show_hidden = true,
+        },
+    },
 }
