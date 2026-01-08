f88bd8fff2267ce08deb94cb7e077271dc4203a6
Ferran Pedrera <ferran.pedrera@technica-engineering.de>
2026-01-08 15:54:58 +0100

    Add lock to powermenu

--- rofi/.config/rofi/powermenu.sh
+++ rofi/.config/rofi/powermenu.sh
@@ -1,10 +1,22 @@
-#! /usr/bin/bash
-#
-chosen=$(printf "󰐥 Power Off\n Restart\n Log out\n󰤄 Suspend" | rofi -dmenu -i -l 4 -theme ~/.config/rofi/config.rasi)
+chosen=$(printf "󰐥 Power Off\n Restart\n Log out\n󰤄 Suspend\n Lock Screen" | rofi -dmenu -i -l 5 -theme ~/.config/rofi/config.rasi)
+
 case "$chosen" in 
-	"󰐥 Power Off") shutdown now;;
-	" Restart") reboot ;;
-    " Log out") i3-msg exit ;;
-    "󰤄 Suspend") systemctl suspend ;;
-	*) exit 1 ;;
+    "󰐥 Power Off")
+        shutdown now
+        ;;
+    " Restart")
+        reboot
+        ;;
+    "󰍃 Log out")
+        i3-msg exit
+        ;;
+    "󰤄 Suspend")
+        systemctl suspend
+        ;;
+    " Lock Screen")
+        ~/.config/i3/lock.sh
+        ;;
+    *)
+        exit 1
+        ;;
 esac
