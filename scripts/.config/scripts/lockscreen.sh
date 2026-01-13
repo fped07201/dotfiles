ce812c8b22e1357b4722b26d22ac99b4b044f930
Ferran Pedrera <ferran.pedrera@technica-engineering.de>
2026-01-13 11:11:53 +0100

    lockscreen script update

--- scripts/.config/scripts/lockscreen.sh
+++ scripts/.config/scripts/lockscreen.sh
@@ -1,38 +1,39 @@
 #!/bin/sh
 
-INSIDE='#0480c1c0'
+INSIDE='#1a1b26dd'
 BLANK='#00000000'
 CLEAR='#ffffff22'
-DEFAULT='#dddddd'
-TEXT='#dddddd'
-WRONG='#880000bb'
-VERIFYING='#9900bbbb'
+DEFAULT='#c0caf5aa'
+TEXT='#7aa2f7ff'
+WRONG='#f7768ebb'
+VERIFYING='#9d7cd8cc'
 
 i3lock-color \
---insidever-color=$CLEAR     \
---ringver-color=$VERIFYING   \
+--insidever-color=$CLEAR      \
+--ringver-color=$VERIFYING    \
 \
---insidewrong-color=$CLEAR   \
---ringwrong-color=$WRONG     \
+--insidewrong-color=$CLEAR    \
+--ringwrong-color=$WRONG      \
 \
 --inside-color=$INSIDE        \
---ring-color=$DEFAULT        \
---line-color=$BLANK          \
---separator-color=$DEFAULT   \
+--ring-color=$TEXT            \
+--line-color=$BLANK           \
+--separator-color=$DEFAULT    \
 \
---verif-color=$TEXT          \
---wrong-color=$TEXT          \
---time-color=$TEXT           \
---date-color=$TEXT           \
---layout-color=$TEXT         \
---keyhl-color=$WRONG         \
---bshl-color=$WRONG          \
+--verif-color=$VERIFYING      \
+--wrong-color=$WRONG          \
+--time-color=$TEXT            \
+--date-color=$TEXT         \
+--layout-color=$DEFAULT       \
+--keyhl-color=$TEXT           \
+--bshl-color=$WRONG           \
 \
---screen 1                   \
+--screen 1                    \
 --blur 10                     \
---clock                      \
---indicator                  \
---time-str="%H:%M:%S"        \
---date-str="%A, %Y-%m-%d"       \
---radius 100               \
-# --keylayout 1                \
+--clock                       \
+--indicator                   \
+--time-str="%H:%M:%S"         \
+--date-str="%A, %Y-%m-%d"     \
+--radius 100                  \
+--ring-width=20
+
