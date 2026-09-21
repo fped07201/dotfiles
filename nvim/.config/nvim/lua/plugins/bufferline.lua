b29ba6badf7a907fe2340529e66818af7d24ec60
Ferran Pedrera <ferran.pedrera@technica-engineering.de>
2026-09-21 15:29:35 +0200

    colorscheme

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -12,9 +12,23 @@ return {
             },
 
             highlights = {
-                -- Empty area around the buffers
-                fill = { fg = "#828BB8", bg = "#222436" },
+                fill                  = { fg = "#828BB8", bg = "#222436" },              -- Empty area around the buffers
+                background            = { fg = "#828BB8", bg = "#222436" },              -- Inactive buffers
+                buffer_visible        = { fg = "#828BB8", bg = "#222436" },              -- Currently visible but not selected
+                buffer_selected       = { fg = "#C8D3F5", bg = "#2F334D", bold = true }, -- Selected buffer
+
+                separator             = { fg = "#222436", bg = "#222436" },              -- Separators between buffers
+                separator_selected    = { fg = "#2F334D", bg = "#2F334D" },
+
+                close_button          = { fg = "#828BB8", bg = "#222436" }, -- Close buttons
+                close_button_visible  = { fg = "#828BB8", bg = "#222436" },
+                close_button_selected = { fg = "#C8D3F5", bg = "#2F334D" },
+
+                modified              = { fg = "#FFC777", bg = "#222436" }, -- Modified buffers
+                modified_visible      = { fg = "#FFC777", bg = "#222436" },
+                modified_selected     = { fg = "#FFC777", bg = "#2F334D" },
             },
+
         })
 
         vim.keymap.set("n", "<leader>bn", "<cmd>BufferLineCycleNext<CR>", { noremap = true, silent = true })
