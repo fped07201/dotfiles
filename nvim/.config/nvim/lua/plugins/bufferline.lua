a8864ab887f80a1b9bdb04ff11cbf51ef8fb0ab2
Ferran Pedrera <ferran.pedrera@technica-engineering.de>
2026-09-21 15:23:39 +0200

    Colorscheme

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -2,15 +2,22 @@ return {
     'akinsho/bufferline.nvim',
     version = "*",
     dependencies = 'nvim-tree/nvim-web-devicons',
+
     config = function()
-        require("bufferline").setup {
+        require("bufferline").setup({
             options = {
                 show_tab_indicators = true,
                 always_show_bufferline = true,
-            }
-        }
+                separator_style = "thin",
+            },
+
+            highlights = {
+                -- Empty area around the buffers
+                fill = { fg = "#828BB8", bg = "#222436" },
+            },
+        })
 
         vim.keymap.set("n", "<leader>bn", "<cmd>BufferLineCycleNext<CR>", { noremap = true, silent = true })
         vim.keymap.set("n", "<leader>bp", "<cmd>BufferLineCyclePrev<CR>", { noremap = true, silent = true })
-    end
+    end,
 }
